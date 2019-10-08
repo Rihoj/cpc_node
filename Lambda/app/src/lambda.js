b82c404c529ce6e18c8dba83be17801c4e3869db
James Ray <james@rayprogramming.com>
2019-10-08 01:22:53 -0400

    Temporary work with mongo. Need to see if we can get AWS SAM to boot up mongo

--- Lambda/app/src/lambda.js
+++ Lambda/app/src/lambda.js
@@ -1,13 +1,19 @@
 // lambda.js
+import "regenerator-runtime";
 import awsServerlessExpress from "aws-serverless-express"
 import app from "./index"
 import {
   connectDb
 } from "./models";
 
-connectDb().then(async () => {
-  const server = awsServerlessExpress.createServer(app)
-  exports.handler = (event, context) => {
+
+const server = awsServerlessExpress.createServer(app)
+
+exports.handler = (event, context) => {
+  connectDb().then( async () => {
     awsServerlessExpress.proxy(server, event, context);
-  }
-});
+  }).catch((err) => {
+    console.log(err);
+    return err;
+  });
+}
